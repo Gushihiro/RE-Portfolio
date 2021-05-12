a355425097c7be2a538895e72d94e429079a1e99
Gushihiro <robinsonhiroto@gmail.com>
2021-05-11 23:54:39 -0700

    working on hero container

---
+++ script.js
@@ -0,0 +1,49 @@
+const bgImage = document.body
+.backgroundImage;
+
+function scrollHandler() {
+    var element = document.querySelector('.parallax');
+  
+    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+    var elementHeight = element.offsetHeight;
+      var scrollTop = document.documentElement.scrollTop;
+      
+      var opacity = 1;
+      
+      if (scrollTop > distanceToTop) {
+          opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+      }
+      
+      if (opacity >= 0) {
+          element.style.opacity = opacity;
+      }}
+  
+  window.addEventListener('scroll', scrollHandler);
+
+var header = document.querySelector('.parallax');
+
+function fadeOutOnScroll(element) {
+	if (!element) {
+		return;
+	}
+	
+	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+	var elementHeight = element.offsetHeight;
+	var scrollTop = document.documentElement.scrollTop;
+	
+	var opacity = 1;
+	
+	if (scrollTop > distanceToTop) {
+		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+	}
+	
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
+}
+
+function scrollHandler() {
+	fadeOutOnScroll(header, bgImage);
+}
+
+window.addEventListener('scroll', scrollHandler);
