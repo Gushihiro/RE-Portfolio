e2364567d2931f5191a80e1295b09c04cdb2f2c3
Gushihiro <robinsonhiroto@gmail.com>
2021-05-12 17:08:31 -0700

    edited site to look more sleek, added content boxes, adjusted gradients and dimmed main logo to match background better

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-const bgImage = document.body
-.backgroundImage;
 
-function scrollHandler() {
-    var element = document.querySelector('.parallax');
+
+// function scrollHandler() {
+//     var element = document.querySelector('.parallax');
   
-    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-    var elementHeight = element.offsetHeight;
-      var scrollTop = document.documentElement.scrollTop;
+//     var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+//     var elementHeight = element.offsetHeight;
+//       var scrollTop = document.documentElement.scrollTop;
       
-      var opacity = 1;
+//       var opacity = 1;
       
-      if (scrollTop > distanceToTop) {
-          opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-      }
+//       if (scrollTop > distanceToTop) {
+//           opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+//       }
       
-      if (opacity >= 0) {
-          element.style.opacity = opacity;
-      }}
+//       if (opacity >= 0) {
+//           element.style.opacity = opacity;
+//       }}
+      
   
-  window.addEventListener('scroll', scrollHandler);
+//   window.addEventListener('scroll', scrollHandler);
 
-var header = document.querySelector('.parallax');
+// var header = document.querySelector('.parallax');
 
-function fadeOutOnScroll(element) {
-	if (!element) {
-		return;
-	}
+// function fadeOutOnScroll(element) {
+// 	if (!element) {
+// 		return;
+// 	}
 	
-	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-	var elementHeight = element.offsetHeight;
-	var scrollTop = document.documentElement.scrollTop;
+// 	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+// 	var elementHeight = element.offsetHeight;
+// 	var scrollTop = document.documentElement.scrollTop;
 	
-	var opacity = 1;
+// 	var opacity = 1;
 	
-	if (scrollTop > distanceToTop) {
-		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-	}
+// 	if (scrollTop > distanceToTop) {
+// 		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+// 	}
 	
-	if (opacity >= 0) {
-		element.style.opacity = opacity;
-	}
-}
+// 	if (opacity >= 0.45) {
+// 		element.style.opacity = opacity;
+// 	}
+// }
 
-function scrollHandler() {
-	fadeOutOnScroll(header, bgImage);
-}
+// function scrollHandler() {
+// 	fadeOutOnScroll(header);
+// }
 
-window.addEventListener('scroll', scrollHandler);
+// window.addEventListener('scroll', scrollHandler);
 
 const observer = new IntersectionObserver(entries => {
     // Loop over the entries
@@ -60,3 +60,16 @@ const observer = new IntersectionObserver(entries => {
   });
   
   observer.observe(document.querySelector('h3'));
+
+  // const observer2 = new IntersectionObserver(entries => {
+  //   // Loop over the entries
+  //   entries.forEach(entry => {
+  //     // If the element is visible
+  //     if (entry.isIntersecting) {
+  //       // Add the animation class
+  //       const bgImage = document.body.style.backgroundImage = "none";
+  //     }
+  //   });
+  // });
+  
+  // observer2.observe(document.querySelector('#vedomytitle'));
