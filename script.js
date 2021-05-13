a965fb854a08d9876b22af10b0226bf6c0b8db9a
Gushihiro <robinsonhiroto@gmail.com>
2021-05-12 21:30:55 -0700

    added encrypted contact form

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // function scrollHandler() {
 //     var element = document.querySelector('.parallax');
   
@@ -73,3 +72,18 @@ const observer = new IntersectionObserver(entries => {
   // });
   
   // observer2.observe(document.querySelector('#vedomytitle'));
+
+  window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('Gushihiro', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
